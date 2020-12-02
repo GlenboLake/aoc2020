eb548dd4ae08b3ea306f2c20dff29f594b01e4ed
Glen Haber <glenbolake@gmail.com>
2020-12-02 00:37:12 -0500

    Update day 1 to use Combinatorics package

--- src/day01/day01.jl
+++ src/day01/day01.jl
@@ -1,35 +1,22 @@
 module Day01
 
 using AoC2020
-
-function part1(nums::Array{Int, 1})
-    for (i, a) in enumerate(nums)
-        for b in nums[i+1:end]
-            if a + b == 2020
-                return a*b
-            end
-        end
-    end
-end
-
-function part2(nums::Array{Int, 1})
-    for (i, a) in enumerate(nums)
-        for (j, b) in enumerate(nums[i+1:end])
-            for c in nums[i+j+1:end]
-                if a+b+c==2020
-                    return a*b*c
-                end
-            end
-        end
-    end
-end
+using Combinatorics
 
 function day01(input::String = readInput(joinpath(@__DIR__, "input.txt")))
     nums = parse.(Int, split(input))
-    part1(nums), part2(nums)
+
+    function check(nums::Array{Int, 1}, n)
+        combos = collect(combinations(nums, n))
+        idx = findfirst(x -> sum(x)==2020, combos)
+        prod(combos[idx])
+    end
+    check(nums, 2), check(nums, 3)
 end
 
-# println(day01("1721 979 366 299 675 1456"))
-println(day01())
+(part1, part2) = day01()
+
+println("Part 1: $part1")
+println("Part 2: $part2")
 
 end # module
