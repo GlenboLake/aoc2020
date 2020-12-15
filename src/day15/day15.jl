a62f83dc589c47aaa6b132911b5049a3ba4028bc
Glen Haber <glenbolake@gmail.com>
2020-12-15 11:25:10 -0500

    Day 15 part 2
    Rewrite playGame() to use a mapping of most recent time
    that a number was said, rather than the full list

--- src/day15/day15.jl
+++ src/day15/day15.jl
@@ -6,22 +6,17 @@ using Test
 input = [1, 17, 0, 10, 18, 11, 6]
 
 function playGame(startingNumbers::Array{Int}, n::Int)
-    history = ones(Int, n) .* -1
-    history[1:length(startingNumbers)] = startingNumbers
-    # @show history
+    history = Dict{Int,Int}([
+        num => i
+        for (i, num) ∈ enumerate(startingNumbers[begin:end-1])
+    ])
+    lastSpoken = startingNumbers[end]
     for i ∈ length(startingNumbers)+1:n
-        if i%10000 == 0
-            @show i
-        end
-        lastSpoken = history[i-1]
-        prev = findlast(x->x==lastSpoken, history[begin:i-2])
-        if prev ≡ nothing
-            history[i] = 0
-        else
-            history[i] = i-1-prev
-        end
+        newNum = lastSpoken ∈ keys(history) ? i - 1 - history[lastSpoken] : 0
+        history[lastSpoken] = i - 1
+        lastSpoken = newNum
     end
-    history[n]
+    lastSpoken
 end
 
 expected = [0,3,6,0,3,3,1,0,4,0]
@@ -30,6 +25,7 @@ for i ∈ 4:length(expected)
 end
 @test playGame([0,3,6], 2020) == 436
 
-@show playGame(input, 2020)
+println("Part 1: $(playGame(input, 2020))")
+println("Part 2: $(playGame(input, 30000000))")
 
 end  # module Day15
