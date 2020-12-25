55387357ad93b6f0b5f82166667ef6cc424638bc
Glen Haber <glenbolake@gmail.com>
2020-12-25 00:32:51 -0500

    Day 24 part 2

--- src/day24/day24.jl
+++ src/day24/day24.jl
@@ -1,7 +1,10 @@
 module Day24
 
 using AoC2020
+using DataStructures
 using Test
+
+TilePos = Vector{Int}
 
 steps = Dict(
     "w"  => [ 0, -2],
@@ -22,9 +25,9 @@ function followPath(path::AbstractString)
     sum(map(m->steps[m.match], eachmatch(dirs, path)))
 end
 
-function part1(input::String = readInput(joinpath(@__DIR__, "input.txt")))
+function setup(input::String)
     tiles = splitLines(input)
-    blackTiles = Set{Vector{Int}}()
+    blackTiles = Set{TilePos}()
     for tile in tiles
         pos = followPath(tile)
         if pos ∈ blackTiles
@@ -33,7 +36,50 @@ function part1(input::String = readInput(joinpath(@__DIR__, "input.txt")))
             push!(blackTiles, pos)
         end
     end
-    length(blackTiles)
+    blackTiles
+end
+
+function neighbors(tile::TilePos)
+    map(d->tile .+ d, values(steps))
+end
+
+function part1(input::String = readInput(joinpath(@__DIR__, "input.txt")))
+    length(setup(input))
+end
+
+function iterate!(tiles::Set{TilePos})
+    neighborCounts = DefaultDict{TilePos,Int}(0)
+    for tile ∈ tiles
+        @assert length(neighbors(tile)) == 6
+        for n ∈ neighbors(tile)
+            neighborCounts[n] += 1
+        end
+        # And make sure this tile is counted even if it has 0 neighbors!
+        neighborCounts[tile]
+    end
+    newTiles = copy(tiles)
+    for (pos, count) ∈ sort(collect(neighborCounts))
+        if pos ∈ tiles && (count == 0 || count > 2)
+            # println("Flipping black tile $pos ($count) to white")
+            pop!(newTiles, pos)
+        elseif pos ∉ tiles && count == 2
+            # println("Flipping white tile $pos ($count) to black")
+            push!(newTiles, pos)
+        else
+            color = pos ∈ tiles ? "black" : "white"
+            # println("Leaving $color tile $pos alone ($count)")
+        end
+    end
+    empty!(tiles)
+    union!(tiles, newTiles)
+end
+
+function part2(input::String = readInput(joinpath(@__DIR__, "input.txt")))
+    floor = setup(input)
+    for day ∈ 1:100
+        iterate!(floor)
+    end
+    length(floor)
 end
 
 sample = """sesenwnenenewseeswwswswwnenewsewsw
@@ -59,5 +105,8 @@ wseweeenwnesenwwwswnew"""
 
 @test part1(sample) == 10
 @show part1()
+
+@test part2(sample) == 2208
+@show part2()
 
 end # module Day24
